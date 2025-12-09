888c49a518f3fcbb5d2c47a9fd52782edeb9e06d
Eric Zhou <yiruiericzhou@gmail.com>
2025-12-09 15:16:42 +0800

    initial commit

---
+++ test.py
@@ -0,0 +1,32 @@
+import subprocess
+
+p = subprocess.Popen(
+    [
+        "/home/ericzhou/Document/GPUErrorDetection/third_party/ripgrep/rg",
+        "test.log",
+        "-e", r"(?:Traceback \(most recent call last\):|File \".*\",\s+line\s+\d+,\s+in).*\n(?:.*\n)*?.*?\S*(?:Error|Exception|Interrupt)(?:\:.*)?$",
+        "--json",
+        "--multiline",
+        "--debug",
+    ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
+stdout, stderr = p.communicate()
+print("# === STDERR ===================")
+print(stderr)
+print("# ==============================")
+
+import json
+import io
+with io.StringIO(stdout.decode("utf-8")) as fin:
+    for line in fin:
+        content = json.loads(line)
+        if content["type"] != "match":
+            continue
+        print(json.dumps(content, indent=2))
+        for submatch_idx, submatch in enumerate(content["data"]["submatches"]):
+            print(f"""Match length: {len(content["data"]["lines"]["text"])}""")
+            print(f"""Submatch start/end: {(submatch["start"], submatch["end"])}""")
+            print(f"""Submatch length: {len(submatch["match"]["text"])}""")
+            print(f"""Submatch start + length: {submatch["start"] + len(submatch["match"]["text"])}""")
